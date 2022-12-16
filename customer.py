0e59397bbab99b1aa0485aae8bc027c759f2c1c6
Nosfer-Projects <114942472+Nosfer-Projects@users.noreply.github.com>
2022-12-16 20:35:01 +0100

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,4 +1,7 @@
 class Customer():
+    """
+    A class for creating a specific client after downloading data from the server
+    """
     def __init__(self, list):
         self.number = list[0]
         self.customer_id = list[1]
