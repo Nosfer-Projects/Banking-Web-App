c2ab6c6905d3813ade5d33b415d3c437d565d90a
Nosfer-Projects <114942472+Nosfer-Projects@users.noreply.github.com>
2022-12-16 20:33:12 +0100

    Update update_sql.py

--- update_sql.py
+++ update_sql.py
@@ -3,6 +3,9 @@ import psycopg2 as pg2
 PASSWORD = 'password to database'
 
 class Sql_update():
+    """
+    After making the transfer, the data on the current account balance are modified by deducting the amount from the transfer.
+    """
     def update(self, customer_id, balance):
         conn = pg2.connect(database='Bank', user='user name',password=PASSWORD, host="localhost", port="5432")
         cur = conn.cursor()
