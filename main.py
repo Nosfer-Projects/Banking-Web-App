45fa6b151bc540f2f11cb5e7983a81151289b521
Nosfer-Projects <114942472+Nosfer-Projects@users.noreply.github.com>
2022-12-16 20:45:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,29 +13,33 @@ app.secret_key = "password to app"
 
 @app.route("/")
 def index():
+    #Homepage
     return render_template("index.html")
 
 @app.route("/about")
 def about():
+    #Section About
     return render_template("about.html")
 
 @app.route("/log_in", methods = ['POST', 'GET'])   
 def log_in():
+    #Page to log in by user
     global BALANCE,customer_data
-
+    #After entering the client's login and password, the data is downloaded for verification
     if request.method == "POST":
         customer_id= request.form["customer_id"]
         password = request.form["password"]
         try:
+            #We checking for data for specific customer
             data = Sql_import()
             customer_data= data.data(customer_id)
-            print(customer_data)
             user_password = customer_data[2]
-
+            #If login and password are correct we will get redirect to account view
             if user_password == password:
                 BALANCE = customer_data[5]    
                 return redirect("account")
         except:
+            #if not we will get again blank fields in login and password, and we will not able to log in.
             return render_template("log_in.html")
 
     return render_template("log_in.html")
@@ -43,6 +47,7 @@ def log_in():
 
 @app.route("/account", methods = ['POST', 'GET'])
 def account():
+    #After logging in the client, you will get a personalized interface
         global BALANCE
         error = None
         customer = Customer(customer_data)
@@ -61,6 +66,7 @@ def account():
                 
 @app.route('/logout')
 def logout():
+    #It will log out our user and go into homepage
     global BALANCE, customer_data
     BALANCE = 0
     customer_data = None
