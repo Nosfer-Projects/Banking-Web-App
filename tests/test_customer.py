ce536622882cb882be1f9b56225cb53a56fbcbcf
Nosfer-Projects <114942472+Nosfer-Projects@users.noreply.github.com>
2023-01-04 20:34:27 +0100

    Adding test

---
+++ tests/test_customer.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from customer import Customer
+
+
+class TestCustomer(TestCase):
+    def setUp(self):
+        self.customer_data = ["1", "123", "pass", "Nick", "Jones", 400 ]
+        self.customer = Customer(self.customer_data)
+    def test_customer_creation(self):
+        self.assertEqual(self.customer.number, "1")
+        self.assertEqual(self.customer.customer_id, "123")
+        self.assertEqual(self.customer.password, "pass")
+        self.assertEqual(self.customer.name, "Nick")
+        self.assertEqual(self.customer.surname, "Jones")
+        self.assertEqual(self.customer.balance, 400)
+
+
+
+
+ 
+
+
