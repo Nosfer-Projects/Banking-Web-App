7d2471e223f1f4b11f4b11d9b459a96cebc8bb59
Kosma Wysokiński <114942472+Nosfer-Projects@users.noreply.github.com>
2023-01-29 14:00:54 +0100

    adding mock and refactor code

--- tests/test_update.py
+++ tests/test_update.py
@@ -4,13 +4,16 @@ from modules.import_sql import Sql_import
 
 class TestUpdate(TestCase):
         def setUp(self):
-            customer = Sql_update()
-            self.update = customer.update("admin", 1000)
             data = Sql_import()
             self.result = data.data("admin")
+            self.mock_obj = MagicMock()
+            self.mock_obj.show.return_value = self.result[-1] 
+            customer = Sql_update()
+            self.update = customer.update("admin", self.mock_obj.show())
+
         def test_data(self):
             self.assertTrue(self.result)
 
         def test_correct_data(self):
-            customer = [1, "admin", "123", "John", "Smith", 1000]
+            customer = [1, "admin", "123", "John", "Smith", self.mock_obj.show()]
             self.assertEqual(self.result, customer)
